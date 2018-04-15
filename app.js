3fec57a0fe6048a42f14491546a5e9cffb1791d2
Brandon Samuel <brandonsamuel72@gmail.com>
2018-04-15 02:50:13 +0000

    made routes modular for bookstore app

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const indexRoutes = require('./routes/index');
+const bookstoreRoutes = require('./routes/bookstore');
+
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extedned: true}));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(indexRoutes);
+app.use("/bookstore", bookstoreRoutes);
 
 app.listen(process.env.PORT || 8080, function(){
   console.log("Bookstore server started...");
