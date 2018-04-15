714b298c81667b44617431684b60ed5a6c2a4acc
Brandon Samuel <brandonsamuel72@gmail.com>
2018-04-15 06:40:01 +0000

    created footer and header partials. created get and post route for adding books

--- routes/bookstore.js
+++ routes/bookstore.js
@@ -3,7 +3,31 @@ const router = express.Router();
 const Bookstore = require("../models/index");
 
 router.get("/", function(req, res){
-  res.send("BOOKSTORE PAGE");
+  Bookstore.find({}, function(err, book){
+    if(err) {
+      console.log(err);
+    } else {
+      res.render("bookstore", {book: book});
+    }
+  })
+});
+
+
+router.post("/", function(req, res){
+  let title = req.body.title;
+  let author = req.body.author;
+  let publisher = req.body.publisher;
+  let description = req.body.description;
+  let image = req.body.image;
+  let category = req.body.category;
+  let newBook = {title: title, author: author, publisher: publisher, description: description, image: image, category: category};
+  Bookstore.create(newBook, function(err, newlyCreated){
+    if(err) {
+      console.log(err);
+    } else {
+      res.redirect("/bookstore");
+    }
+  })
 });
 
 module.exports = router;
